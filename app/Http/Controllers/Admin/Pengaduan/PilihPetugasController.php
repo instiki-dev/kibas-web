8e1507c1f78094c4838102b3b3a59cb9560bc8b4
MeiSastraJayadi <kadekmeisastra13@gmail.com>
2023-08-13 13:16:44 +0800

    add logic to push notification

--- app/Http/Controllers/Admin/Pengaduan/PilihPetugasController.php
+++ app/Http/Controllers/Admin/Pengaduan/PilihPetugasController.php
@@ -18,50 +18,55 @@ class PilihPetugasController extends Controller
      */
     public function __invoke(Request $request, Pengaduan $pengaduan, Pegawai $petugas)
     {
-        $url = 'https://fcm.googleapis.com/fcm/send';
-        $serverKey = env('SERVER_KEY');
 
         $data = [
             "petugas_id" => (int)$request -> petugas_id,
             "status" => 2
         ];
 
-        $token = [$petugas -> user -> device_token];
-
-
         Pengaduan::where('id', $pengaduan -> id) -> update($data);
 
-        $message = ["registration_ids" => $token];
-        $message["notification"] = [
-            "title" => 'Pengaduan Baru',
-            "body" => 'Segera proses pengaduan dari pelanggan'
-        ];
+        if($petugas -> user -> device_token) {
+            $url = 'https://fcm.googleapis.com/fcm/send';
+            $serverKey = env('SERVER_KEY');
+            $token = [$petugas -> user -> device_token];
 
-        $encodedData = json_encode($message);
+            $message = ["registration_ids" => $token];
+            $message["notification"] = [
+                "title" => 'Pengaduan Baru',
+                "body" => 'Segera proses pengaduan dari pelanggan'
+            ];
 
-         $headers = [
-            'Authorization:key=' . $serverKey,
-            'Content-Type: application/json',
-        ];
+            $encodedData = json_encode($message);
 
-        $ch = curl_init();
+             $headers = [
+                'Authorization:key=' . $serverKey,
+                'Content-Type: application/json',
+            ];
 
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-        curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
-        // Disabling SSL Certificate
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $encodedData);
+            $ch = curl_init();
 
-          $result = curl_exec($ch);
-        if ($result === FALSE) {
-            // die('Curl failed: ' . curl_error($ch));
-            return redirect() -> route('pengaduan') -> with('errorMessage', "Gagal memberikan notif kepada pelanggan");
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+            curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+            // Disabling SSL Certificate
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $encodedData);
+
+              $result = curl_exec($ch);
+            if ($result === FALSE) {
+                // die('Curl failed: ' . curl_error($ch));
+                curl_close($ch);
+                return redirect() -> route('pengaduan') -> with('errorMessage', "Gagal memberikan notif kepada pelanggan");
+            }
+            curl_close($ch);
         }
-        curl_close($ch);
+
+
+
 
         $riwayat = [
             "pengaduan_id" => $pengaduan -> id,
