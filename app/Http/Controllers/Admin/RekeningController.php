8d2169215cecdbd7b1cf4cd84bb291367b9ec8cc
MeiSastraJayadi <kadekmeisastra13@gmail.com>
2023-10-29 14:12:11 +0800

    change in rekening controller

--- app/Http/Controllers/Admin/RekeningController.php
+++ app/Http/Controllers/Admin/RekeningController.php
@@ -27,6 +27,7 @@ class RekeningController extends Controller
                     -> with('pelanggan:id,nama_pelanggan')
                     -> select('id', 'no_rekening', 'pelanggan_id')
                     -> get();
+            dd($data[0]);
             return DataTables::of($data)
                 ->addIndexColumn()
                 ->addColumn('aksi', function($row){
