c3d8126953034bd56cf2b1daaaa39316d94b8fe1
MeiSastraJayadi <kadekmeisastra13@gmail.com>
2023-11-02 15:13:41 +0800

    change structure in baca meter table

--- app/Models/BacaMeter.php
+++ app/Models/BacaMeter.php
@@ -10,8 +10,4 @@ class BacaMeter extends Model
     use HasFactory;
     protected $guarded = ['id', 'created_at', 'updated_at'];
 
-
-    public function rekening() {
-        return Rekening::where('no_rekening', $this -> no_rekening) -> first();
-    }
 }
