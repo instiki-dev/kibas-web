20ec14908565581b8a88c18c24b418ebb58344f2
MeiSastraJayadi <kadekmeisastra13@gmail.com>
2023-11-05 19:20:55 +0800

    change in here

--- app/Http/Controllers/Admin/Pengumuman/TambahPengumumanDBController.php
+++ app/Http/Controllers/Admin/Pengumuman/TambahPengumumanDBController.php
@@ -118,15 +118,15 @@ class TambahPengumumanDBController extends Controller
 
         $ch = curl_init();
 
-        // curl_setopt($ch, CURLOPT_URL, $url);
-        // curl_setopt($ch, CURLOPT_POST, true);
-        // curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        // curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-        // curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
         // // Disabling SSL Certificate
-        // curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        // curl_setopt($ch, CURLOPT_POSTFIELDS, $encodedData);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $encodedData);
 
         //   $result = curl_exec($ch);
         // if ($result === FALSE) {
