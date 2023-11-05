9da2da598ee7aa43f2047ce262b0ab99bc01d906
MeiSastraJayadi <kadekmeisastra13@gmail.com>
2023-11-05 17:01:11 +0800

    integration to get tagihan from limasakti

--- app/Http/Controllers/API/Pelanggan/Auth/LoginController.php
+++ app/Http/Controllers/API/Pelanggan/Auth/LoginController.php
@@ -7,6 +7,7 @@ use App\Models\Rekening;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Validator;
 use Nette\Schema\ValidationException;
 
@@ -18,6 +19,15 @@ class LoginController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
+    public function getTagihan(String $noRekening) {
+        return Http::withHeaders([
+                'Authorization' => 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoibGF5YW5hbnBlbCIsInBhc3MiOiIwMzhlM2U0YWQ3NDQ1M2E3MjE1M2ZiYmYzMmE2MDU3MyIsInRhbmdnYWwiOiIyMDIxLTAyLTA1IDEyOjQ3OjAwIn0.iSE4SGsAmnKh6E7tgf7rG4qjzYbmojY-jkhw0SFktmA',
+                'Accept' => 'application/json'
+            ])
+            ->get('https://apikabbangli.limasakti.co.id/api/loket-datatagihan/'.$noRekening);
+    }
+
+
     public function __invoke(Request $request)
     {
         try {
--- app/Http/Controllers/API/Pelanggan/GetTagihanController.php
+++ app/Http/Controllers/API/Pelanggan/GetTagihanController.php
@@ -7,6 +7,7 @@ use App\Models\Rekening;
 use App\Models\Tagihan;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class GetTagihanController extends Controller
 {
@@ -16,9 +17,54 @@ class GetTagihanController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
+
+    public function getTagihan(String $noRekening) {
+        return Http::withHeaders([
+                'Authorization' => 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoibGF5YW5hbnBlbCIsInBhc3MiOiIwMzhlM2U0YWQ3NDQ1M2E3MjE1M2ZiYmYzMmE2MDU3MyIsInRhbmdnYWwiOiIyMDIxLTAyLTA1IDEyOjQ3OjAwIn0.iSE4SGsAmnKh6E7tgf7rG4qjzYbmojY-jkhw0SFktmA',
+                'Accept' => 'application/json'
+            ])
+            ->get('https://apikabbangli.limasakti.co.id/api/loket-datatagihan/'.$noRekening);
+    }
+
     public function __invoke(Request $request, Rekening $rekening)
     {
         try {
+            $dt = $this -> getTagihan('0'.$rekening -> no_rekening);
+            $data = json_decode($dt);
+            if ($data -> data) {
+                foreach($data -> data as $item) {
+                    $waktu = explode(" ", $item -> bulan);
+                    $bulan = $waktu[0];
+                    if ($bulan == 'Okt') {
+                        $bulan = 10;
+                    } else {
+                        $bulan = 11;
+                    }
+                    $tahun = (int)$waktu[1];
+                    $tagihan = Tagihan::where([
+                        ["rekening_id", $rekening -> id],
+                        ["bulan", $bulan],
+                        ["tahun", $tahun],
+                    ])
+                    -> first();
+
+                    if (!$tagihan) {
+                        $create = [
+                            "no_tagihan" => $item -> tagihan,
+                            "pelanggan_id" => $rekening -> pelanggan -> id,
+                            "rekening_id" => $rekening -> id,
+                            "bulan" => $bulan,
+                            "tahun" => $tahun,
+                            "tgl_jatuh_tempo" => now(),
+                            "status" => 1,
+                            "nominal" => $item -> tagihan,
+                            "kilometer" => $item -> pakai,
+                            "denda" => $item -> denda
+                        ];
+                        Tagihan::create($create);
+                    }
+                }
+            }
             if ($request -> query('dashboard')) {
                 $tagihan = Tagihan::where([
                     ['rekening_id', $rekening -> id],
