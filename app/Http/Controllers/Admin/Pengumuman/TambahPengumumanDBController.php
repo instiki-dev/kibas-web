eaa81b0e7419bdec8b40962b164f5d29587fa380
MeiSastraJayadi <kadekmeisastra13@gmail.com>
2023-11-05 18:48:30 +0800

    debug

--- app/Http/Controllers/Admin/Pengumuman/TambahPengumumanDBController.php
+++ app/Http/Controllers/Admin/Pengumuman/TambahPengumumanDBController.php
@@ -118,23 +118,23 @@ class TambahPengumumanDBController extends Controller
 
         $ch = curl_init();
 
-        // curl_setopt($ch, CURLOPT_URL, $url);
-        // curl_setopt($ch, CURLOPT_POST, true);
-        // curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        // curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-        // curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
         // Disabling SSL Certificate
-        // curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        // curl_setopt($ch, CURLOPT_POSTFIELDS, $encodedData);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $encodedData);
 
-        //   $result = curl_exec($ch);
-        // if ($result === FALSE) {
+          $result = curl_exec($ch);
+        if ($result === FALSE) {
             // die('Curl failed: ' . curl_error($ch));
-        //     return redirect() -> route('pengumuman') -> with('errorMessage', "Gagal memberikan notif kepada pelanggan");
-        // }
+            return redirect() -> route('pengumuman') -> with('errorMessage', "Gagal memberikan notif kepada pelanggan");
+        }
         // Close connection
-        // curl_close($ch);
+        curl_close($ch);
 
         // dd($result);
 
