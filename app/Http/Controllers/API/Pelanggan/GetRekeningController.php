3c10e5194024739c704406e0a15d285f5bb137d4
I Made Mei Sastra Jayadi <meisastra251@gmail.com>
2024-08-09 23:03:24 +0800

    test pull

--- app/Http/Controllers/API/Pelanggan/GetRekeningController.php
+++ app/Http/Controllers/API/Pelanggan/GetRekeningController.php
@@ -16,6 +16,7 @@ class GetRekeningController extends Controller
      */
     public function __invoke(Request $request, Rekening $rekening)
     {
+        // Ini Comment
         $user = auth('sanctum') -> user();
         $data = [
             "id" => $rekening -> id,
